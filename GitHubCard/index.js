ed5dbd1cd262df4e6f132dd95b5a6e71cca52df8
ryankayne <coope1rk@gmail.com>
2019-10-31 17:33:18 -0400

    Axios set, building component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/ryankayne')
+  .then(response => {
+    console.log(response);
+    let myInfo = response.data;
+        
+  })
+  .catch( error => {
+    //
+    console.log(error);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -45,6 +55,38 @@ const followersArray = [];
 </div>
 
 */
+
+function random(object) {
+
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const github = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(github);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+  
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
